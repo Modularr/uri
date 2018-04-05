3689eaf7b08b9030a5a15d3fa4623d639c657ecb
modbot <git@modularr.com>
2018-04-05 23:09:06 +0200

    Formating
    Clean up code, proper indenting

--- src/URI.php
+++ src/URI.php
@@ -1,51 +1,45 @@
-<?php class URI
+<?php
+class URI
 {
-	public function __construct($folder=null)
-	{
-		$this->folder = $folder;
-		$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-		if(!empty($this->folder))
-		{
-			$this->uri_string = explode($this->folder, $request);
-			$this->uri_string = $this->uri_string['1'];
-		}
-		else
-		{
-			$this->uri_string = $request;
-		}
-		
-		$this->segments = explode('/', substr($this->uri_string(),1));
-	}
-	
-	public function slash_segment($n, $where = 'trailing', $which = 'segment')
-	{
-		$leading = $trailing = '/';
-		
-		if ($where === 'trailing')
-		{
-			$leading	= '';
-		}
-		elseif ($where === 'leading')
-		{
-			$trailing	= '';
-		}
-		
-		return $leading.$this->$which($n).$trailing;
-	}
-	
-	public function segment($n, $no_result = NULL)
-	{
-		$n = $n-1;
-		return (!empty($this->segments[$n])) ? $this->segments[$n] : $no_result;
-	}
-	
-	public function total_segments()
-	{
-		return count($this->segments);
-	}
-	
-	public function uri_string()
-	{
-		return $this->uri_string;
-	}
+    public function __construct($folder=null)
+    {
+        $this->folder = $folder;
+        $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        if(!empty($this->folder)) {
+            $this->uri_string = explode($this->folder, $request);
+            $this->uri_string = $this->uri_string['1'];
+        } else {
+            $this->uri_string = $request;
+        }
+        $this->segments = explode('/', substr($this->uri_string(),1));
+    }
+    
+    public function slash_segment($n, $where = 'trailing', $which = 'segment')
+    {
+        $leading = $trailing = '/';
+        
+        if ($where === 'trailing') {
+            $leading = '';
+        } elseif ($where === 'leading') {
+            $trailing = '';
+        }
+        
+        return $leading.$this->$which($n).$trailing;
+    }
+    
+    public function segment($n, $no_result = null)
+    {
+        $n = $n-1;
+        return (!empty($this->segments[$n])) ? $this->segments[$n] : $no_result;
+    }
+    
+    public function total_segments()
+    {
+        return count($this->segments);
+    }
+    
+    public function uri_string()
+    {
+        return $this->uri_string;
+    }
 }
