ec1edeffe07d83920b1af805e9f6519bfd6ef1b6
David <388467+ISTPdev@users.noreply.github.com>
2021-09-30 22:59:18 +0200

    Update URI.php

--- src/URI.php
+++ src/URI.php
@@ -1,21 +1,22 @@
 <?php
 class URI
 {
-    public function __construct($folder=null)
-    {
+    public function __construct($folder=null) {
+        
         $this->folder = $folder;
         $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        
         if(!empty($this->folder)) {
             $this->uri_string = explode($this->folder, $request);
             $this->uri_string = $this->uri_string['1'];
         } else {
             $this->uri_string = $request;
         }
+        
         $this->segments = explode('/', substr($this->uri_string(),1));
     }
     
-    public function slash_segment($n, $where = 'trailing', $which = 'segment')
-    {
+    public function slash_segment($n, $where = 'trailing', $which = 'segment') {
         $leading = $trailing = '/';
         
         if ($where === 'trailing') {
@@ -24,22 +25,19 @@ class URI
             $trailing = '';
         }
         
-        return $leading.$this->$which($n).$trailing;
+        return $leading . $this->$which($n) . $trailing;
     }
     
-    public function segment($n, $no_result = null)
-    {
+    public function segment($n, $no_result = null) {
         $n = $n-1;
         return (!empty($this->segments[$n])) ? $this->segments[$n] : $no_result;
     }
     
-    public function total_segments()
-    {
+    public function total_segments() {
         return count($this->segments);
     }
     
-    public function uri_string()
-    {
+    public function uri_string() {
         return $this->uri_string;
     }
 }
